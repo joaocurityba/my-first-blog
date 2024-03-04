4337ae55ffb5a053209a6a9eeb256e5ba3656d9b
joaocurityba <jcurityba@gmail.com>
2024-03-04 14:50:41 -0300

    Mais uma tentativa de tipar css

--- meusite/settings.py
+++ meusite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import mimetypes
+
 mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
